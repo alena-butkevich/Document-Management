0bbee03e1747656ae3106afc5322c2fffdb0c2ae
ButkevichAlena <alena-butkevich@mail.ru>
2015-11-08 19:43:36 +0300

    no message

--- DocumentFlow/DocumentFlow/Controllers/MainController.cs
+++ DocumentFlow/DocumentFlow/Controllers/MainController.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DocumentFlow.Models;
 
 namespace DocumentFlow.Controllers
 {
     public class MainController : Controller
     {
-        // GET: Main
+         // GET: Main
         public ActionResult Index()
         {
             return View();
+        }
+
+        DocumentContext db = new DocumentContext();
+
+        public List<Document> getInputDocuments(int userid)
+        {
+            List<Document> list = new List<Document>();
+            
+                foreach (Document d in db.documents)
+                {
+                    if (userid == d.userid)
+                    {
+                        list.Add(d);
+                    }
+                }
+                return list;
+      }
+
+        public List<Document> getOutputDocuments(int currentid)
+        {
+            List<Document> list = new List<Document>();
+            foreach (Document d in db.documents)
+            {
+                if (currentid == d.currentid)
+                {
+                    list.Add(d);
+                }
+            }
+            return list;
         }
     }
 }
---
+++ DocumentFlow/DocumentFlow/Models/DocumentContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace DocumentFlow.Models
+{
+    public class DocumentContext: DbContext
+    {
+        public DbSet<Document> documents;
+    }
+}
